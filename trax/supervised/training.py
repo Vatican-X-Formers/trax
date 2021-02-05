51b478d86a9b491c7a91095121291c5380248dbb
syzymon <s.tworkowski@student.uw.edu.pl>
2021-02-05 18:22:56 +0100

    Fix logging formatting

--- trax/supervised/training.py
+++ trax/supervised/training.py
@@ -678,7 +678,7 @@ class Loop:
         metric_values, _ = evaluator.metrics_fn(
             batch, metrics_weights, metrics_state, rng)
         end = timer()
-        logging.info("batch time: {}", end - start)
+        logging.info("batch time: {}", str(end - start))
         sums += metric_values
 
       print_statistics_counter(hashes)
