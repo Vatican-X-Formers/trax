e93ce9a1bda9be6d9b3a19fa1aa4f614d0b25332
mvxxx <michal.tyrolski@gmail.com>
2021-04-02 19:02:00 +0200

    step 3 or even 4

---
+++ im_model_gen_step3.py
@@ -0,0 +1,96 @@
+import trax
+import gin
+import os
+from trax.models import FunnelTransformerLM
+
+"""
+STEPS
+0. go with im32 gen
+1. Load and go with im32 gen
+2. Load and go with cf10 gen
+3. Load, replace weights and go with cf10 cls
+"""
+
+"""
+FunnelTransformerLM.d_ff = 256
+FunnelTransformerLM.d_model = 64
+FunnelTransformerLM.dropout = 0.04
+FunnelTransformerLM.ff_activation = @trax.layers.FastGelu
+FunnelTransformerLM.n_funnel_blocks = (12,)
+FunnelTransformerLM.n_heads = 1
+FunnelTransformerLM.shorten_factors = (3,)
+FunnelTransformerLM.vanilla_layers = (1, 1)
+FunnelTransformerLM.vocab_size = 256
+"""
+
+n_devices = 8
+total_batch_size = n_devices
+
+model = FunnelTransformerLM(
+    d_ff = 256,
+    d_model = 64,
+    dropout = 0.04,
+    ff_activation = trax.layers.FastGelu,
+    n_funnel_blocks = (12,),
+    n_heads = 1,
+    shorten_factors = (3,),
+    vanilla_layers = (1,1),
+    vocab_size = 256
+)
+
+cls_model = model = FunnelTransformerLM(
+    d_ff = 256,
+    d_model = 64,
+    dropout = 0.04,
+    ff_activation = trax.layers.FastGelu,
+    n_funnel_blocks = (12,),
+    n_heads = 1,
+    shorten_factors = (3,),
+    vanilla_layers = (1,1),
+    vocab_size = 256,
+    n_classes=10
+)
+
+_, _ = model.init_from_file(
+        '/content/train_dir/model_10.pkl.gz',
+        weights_only=True,
+        #input_signature=trax.shapes.ShapeDtype((total_batch_size, 1), dtype=np.int32)
+)
+print(f'model.weights len = {len(model.weights)}, cls_model.weights len = {len(cls_model.weights)}')
+cls_model.weights = model.weights[:-1] + cls_model.weights[-1:]
+
+def vatican_stream():
+    streams = trax.data.tf_inputs.data_streams(
+        data_dir = None,
+        dataset_name = 'cifar10',
+        input_name = 'image',
+        target_name = 'label',
+        bare_preprocess_fn = trax.data.tf_inputs.downsampled_imagenet_flatten_bare_preprocess_gen
+    )
+
+    return trax.data.inputs.batcher(streams, variable_shapes=False,
+                        batch_size_per_device=1,
+                        eval_batch_size=1)
+
+train_stream = itertools.cycle(vatican_stream().train_stream(n_devices))
+eval_stream = itertools.cycle(vatican_stream().eval_stream(n_devices))
+
+reg_task = trax.supervised.training.TrainTask(
+    labeled_data=train_stream,
+    loss_layer=trax.layers.WeightedCategoryCrossEntropy(),
+    optimizer=trax.optimizers.adam.Adam(0.001),
+)
+reg_eval_task = trax.supervised.training.EvalTask(
+    labeled_data=eval_stream,
+    metrics=[trax.layers.WeightedCategoryCrossEntropy(), trax.layers.Accuracy()]
+)
+
+training_session = trax.supervised.training.Loop(
+    cls_model,
+    tasks=[reg_task],
+    eval_tasks=[reg_eval_task],
+    eval_at=lambda step_n: step_n % 20 == 0,
+)
+
+
+training_session.run(n_steps=100)
