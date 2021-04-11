09ce707fd76de7d08047e7d4b756d7731f1a6d07
syzymon <s.tworkowski@student.uw.edu.pl>
2021-04-11 16:58:46 +0200

    Chunking - initial implementation

--- trax/layers/research/rel_attention.py
+++ trax/layers/research/rel_attention.py
@@ -64,8 +64,8 @@ def RelativeAttentionWrapper(
         location_bias_layer=None,
         n_raw_tokens_generated=None,
         max_inference_length=3072,
-        chunk_len=None,
-        chunk_offset=None):
+        chunk_len=128,
+        chunk_offset=64):
   # TODO: this will be attn_type for DecoderBlock
   del d_v, causal, masked, output_dropout
   return RelativeAttentionLMLayer(d_qk * n_heads, context_bias_layer,
@@ -74,6 +74,8 @@ def RelativeAttentionWrapper(
                                   dropout=attention_dropout,
                                   n_raw_tokens_generated=n_raw_tokens_generated,
                                   max_inference_length=max_inference_length,
+                                  chunk_len=chunk_len,
+                                  chunk_offset=chunk_offset,
                                   mode=mode)
 
 
@@ -87,6 +89,8 @@ def RelativeAttentionLayer(d_feature,
                            dropout=0.0,
                            n_raw_tokens_generated=1,
                            max_inference_length=3072,
+                           chunk_len=None,
+                           chunk_offset=None,
                            mode='train'):
   """Returns a layer that maps (q, k, v, masks) to (activations, masks).
 
@@ -146,6 +150,8 @@ def RelativeAttentionLayer(d_feature,
           dropout=dropout,
           n_raw_tokens_generated=n_raw_tokens_generated,
           max_inference_length=max_inference_length,
+          chunk_len=chunk_len,
+          chunk_offset=chunk_offset,
           mode=mode),
       core.Dense(d_feature),
   )
@@ -161,6 +167,8 @@ def RelativeAttentionLMLayer(d_feature,
                              dropout=0.0,
                              n_raw_tokens_generated=1,
                              max_inference_length=3072,
+                             chunk_len=None,
+                             chunk_offset=None,
                              mode='train'):
   """Returns a layer that maps (q, k, v) to (activations).
 
@@ -196,6 +204,8 @@ def RelativeAttentionLMLayer(d_feature,
       dropout=dropout,
       n_raw_tokens_generated=n_raw_tokens_generated,
       max_inference_length=max_inference_length,
+      chunk_len=chunk_len,
+      chunk_offset=chunk_offset,
       mode=mode)
 
   return cb.Serial(
@@ -236,6 +246,8 @@ class RelativeAttention(base.Layer):
                dropout=0.0,
                n_raw_tokens_generated=1,
                max_inference_length=3072,
+               chunk_len=None,
+               chunk_offset=None,
                mode='train'):
     """Returns a new PureAttention instance.
 
@@ -258,6 +270,8 @@ class RelativeAttention(base.Layer):
     self._dropout = dropout
     self._n_raw_tokens_generated = n_raw_tokens_generated
     self._max_len = max_inference_length
+    self._chunk_len = chunk_len
+    self._chunk_offset = chunk_offset
     self._mode = mode
 
   def forward(self, inputs):
@@ -290,7 +304,10 @@ class RelativeAttention(base.Layer):
         separate_cls=self._separate_cls,
         dropout=self._dropout,
         mode=self._mode,
-        rng=self.rng)
+        rng=self.rng,
+        chunk_len=self._chunk_len,
+        chunk_offset=self._chunk_offset
+    )
     if self._mode == 'viz':
       self.state = dots
     merged_results = MergeHeads(
@@ -350,7 +367,8 @@ class RelativeAttention(base.Layer):
 
 
 def DotProductAttention(queries, keys, values, pos_emb, context_bias,
-                        location_bias, mask, separate_cls, dropout, mode, rng):
+                        location_bias, mask, separate_cls, dropout, mode, rng,
+                        chunk_len, chunk_offset=0):
   """Computes new activations via masked attention-weighted sum of values.
 
   This function is the core of the attention mechanism. It:
@@ -377,35 +395,89 @@ def DotProductAttention(queries, keys, values, pos_emb, context_bias,
     Per-head activations resulting from masked per-head attention-weighted
     sum of per-head values.
   """
-  d_feature = queries.shape[-1]
-  keys_len, queries_len = keys.shape[-2], queries.shape[-2]
-  funnel_factor, is_upsampling = calc_funnel_ratio(keys_len, queries_len)
+  bs, nh, original_l, d_feature = queries.shape
 
-  ac = jnp.einsum('bnid,bnjd->bnij', queries + context_bias, keys)
-  bd = jnp.einsum('bnid,jnd->bnij', queries + location_bias, pos_emb)
+  def _calc_attn_scores(q, k):
+    ac = jnp.einsum('bnid,bnjd->bnij', q + context_bias, k)
+    bd = jnp.einsum('bnid,jnd->bnij', q + location_bias,
+                    pos_emb[:2 * k.shape[2] - 1, ...])
 
-  if mode != 'predict':
-    bd = _fast_matrix_shift(bd, funnel_factor, is_upsampling)
+    if mode != 'predict':
+      bd = _fast_matrix_shift(bd)
 
-  if separate_cls:
-    # Masking out location part of attention for cls token
-    bd = bd.at[:, :, :, 0].set(0)
-    bd = bd.at[:, :, 0, :].set(0)
+    if separate_cls:
+      # Masking out location part of attention for cls token
+      bd = bd.at[:, :, :, 0].set(0)
+      bd = bd.at[:, :, 0, :].set(0)
 
-  dots = (ac + bd) / jnp.sqrt(d_feature)
-  if mask is not None:
+    dots = (ac + bd) / jnp.sqrt(d_feature)
+
+    seq_len = q.shape[2]
+    mask = jnp.tril(jnp.ones((seq_len, seq_len), dtype=jnp.bool_))
     dots = jnp.where(mask, dots, jnp.full_like(dots, -1e9))
-  # Softmax.
-  dots = jnp.exp(dots - fastmath.logsumexp(dots, axis=-1, keepdims=True))
-  if dropout >= 1.0:
-    raise ValueError('Dropout rates must be lower than 1.')
-  if dropout is not None and dropout > 0.0 and mode == 'train':
-    keep = fastmath.random.bernoulli(rng, 1.0 - dropout, dots.shape)
-    dots = jnp.where(keep, dots / (1.0 - dropout), jnp.zeros_like(dots))
-  out = jnp.matmul(dots, values)
+
+    # Softmax.
+    dots = jnp.exp(dots - fastmath.logsumexp(dots, axis=-1, keepdims=True))
+    if dropout >= 1.0:
+      raise ValueError('Dropout rates must be lower than 1.')
+    if dropout is not None and dropout > 0.0 and mode == 'train':
+      keep = fastmath.random.bernoulli(rng, 1.0 - dropout, dots.shape)
+      dots = jnp.where(keep, dots / (1.0 - dropout), jnp.zeros_like(dots))
+
+    return dots
+
+  if chunk_len is None or mode == 'predict':
+    dots = _calc_attn_scores(queries, keys)
+    out = jnp.matmul(dots, values)
+  else:
+    n_chunks = original_l // chunk_len
+
+    def chunk_split(v):
+      chunked_shape = (bs, nh, n_chunks, chunk_len, d_feature)
+      v = jnp.reshape(v, chunked_shape)
+      v = v.swapaxes(1, 2)
+      return jnp.reshape(v, (bs * n_chunks, nh, chunk_len, d_feature))
+
+    def chunk_join(v):
+      swapped_shape = (bs, n_chunks, nh, chunk_len, d_feature)
+      v = jnp.reshape(v, swapped_shape)
+      v = v.swapaxes(1, 2)
+      return jnp.reshape(v, (bs, nh, original_l, d_feature))
+
+    if chunk_offset == 0:
+      queries, keys, values = map(chunk_split, [queries, keys, values])
+      chunked_dots = _calc_attn_scores(queries, keys)
+      chunked_result = jnp.matmul(chunked_dots, values)
+      out = chunk_join(chunked_result)
+    else:
+      pre_len, post_len = chunk_offset, chunk_len - chunk_offset
+      total_len = queries.shape[2]
+
+      def split_along_l(v, mid_start, mid_end, end):
+        pre = jnp.take(v, indices=range(mid_start), axis=2)
+        mid = jnp.take(v, indices=range(mid_start, mid_end), axis=2)
+        post = jnp.take(v, indices=range(mid_end, end), axis=2)
+        return pre, mid, post
+
+      def permute(v):
+        pre, mid, post = split_along_l(v, pre_len, total_len - post_len,
+                                       total_len)
+        return jnp.concatenate([mid, pre, post], axis=2)
+
+      def unpermute(v):
+        mid, pre, post = split_along_l(v, total_len - chunk_len,
+                                       total_len - post_len, total_len)
+        return jnp.concatenate([pre, mid, post], axis=2)
+
+      queries, keys, values = map(lambda x: chunk_split(permute(x)),
+                                  [queries, keys, values])
+      permuted_dots = _calc_attn_scores(queries, keys)
+      permuted_out = chunk_join(jnp.matmul(permuted_dots, values))
+
+      out = unpermute(permuted_out)
+
   out = out.astype(jnp.float32)
-  dots = dots.astype(jnp.float32)
-  return out, dots
+  return out, None  # We don't store full dots matrix
 
 
 class PositionalEmbeddings(base.Layer):
@@ -533,7 +605,7 @@ def calc_funnel_ratio(keys_len, queries_len):
   return funnel_factor, is_upsampling
 
 
-def _fast_matrix_shift(x, funnel_factor, is_upsampling=False):
+def _fast_matrix_shift(x):
   """Fast matrix shift.
 
   Implements necessary shift for relative positional attention calculations.
@@ -552,12 +624,7 @@ def _fast_matrix_shift(x, funnel_factor, is_upsampling=False):
   #  shift: i-th row is shifted by i * shift elements to the left
   #  k: after shift, we pick every kth element
 
-  if is_upsampling:
-    k = funnel_factor
-    shift = 1
-  else:
-    k = 1
-    shift = funnel_factor
+  k, shift = 1, 1
 
   bsz, n_head = x.shape[0], x.shape[1]
   qlen, klen = x.shape[2], (x.shape[3] + 1) // 2
