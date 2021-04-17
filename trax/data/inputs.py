4ef880ec10c3b278f7b70516a7bf888ac57c7dd0
mvxxx <michal.tyrolski@gmail.com>
2021-04-17 19:01:31 +0200

    overwrite boundaries

--- trax/data/inputs.py
+++ trax/data/inputs.py
@@ -525,7 +525,8 @@ def bucket_by_length(generator, length_fn, boundaries, batch_sizes,
   Yields:
     An input batch, which comes from one of the buckets.
   """
-  buckets = [[] for _ in range(len(batch_sizes) + 1)]
+  boundaries = [1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]
+  buckets = [[] for _ in range(len(batch_sizes))]
   boundaries = boundaries + [math.inf]  # Max boundary is unlimited.
   for example in generator:
     length = length_fn(example)
